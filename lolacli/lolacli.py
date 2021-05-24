3c1f4cfeb216febd68fda46f3a1bff071b544077
Arghya <arghyasarkar.joker@gmail.com>
2021-05-24 18:24:01 +0530

    0.1.8

--- lolacli/lolacli.py
+++ lolacli/lolacli.py
@@ -1,6 +1,9 @@
 import click
-import subprocess as sb
-from runfile import rn as run
+import subprocess as sb 
+
+
+def run(command):
+    sb.run(command, shell=True)
 
 apps='''gimp
 da-vinci-resolve
@@ -87,14 +90,14 @@ nodejs'''
 	
 
 @click.group()
-@click.version_option('0.1.4', prog_name= 'Lola')
+@click.version_option('0.1.7', prog_name= 'Lola')
 def main():
 	'''I am Lola! Your assistant who can help you setup your Linux in an easy way! You can know more about me in https://github.com/arghyagod-coder/lola.
 
 I can help you install apps through terminal, and you need to know almost nothing about the terminal to do so! Just simple prompts will be enough'''
 	pass
 
-@main.command('check-apps')
+@main.command('check-apps', help='Check the list of supported apps!')
 def checkapps():
 	print(apps)
 
@@ -102,4 +105,4 @@ def checkapps():
 @click.argument('filesl', nargs=-1)
 def install(filesl): 
 	for files in filesl:
-		run(f'cd ~/Downloads; wget https://raw.githubusercontent.com/arghyagod-coder/lola/master/lola/scripts/{files}.sh; bash {files}.sh; rm {files}.sh')
+		run(f'cd ~/Downloads; wget https://raw.githubusercontent.com/arghyagod-coder/lola/blob/master/lolacli/scripts/{files}.sh; bash {files}.sh; rm {files}.sh')
--- tests/test_lola.py
+++ tests/test_lola.py
@@ -1,4 +1,4 @@
 from lolacli import __version__
 
 def test_version():
-    assert __version__=='0.1.4'
+    assert __version__=='0.1.8'
