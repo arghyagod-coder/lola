095396a91f55988ca45cd13ca35330ef2ba2eaed
Shravan Asati <dev.shravan@protonmail.com>
2021-05-25 22:37:42 +0530

    ✨ changed the check-apps command to list, improved installation

--- lolacli/lolacli.py
+++ lolacli/lolacli.py
@@ -1,11 +1,15 @@
 import click
-import subprocess as sb 
+import requests
+import os
+import subprocess as sb
 
 
 def run(command):
     sb.run(command, shell=True)
 
-apps='''gimp
+
+apps = '''
+gimp
 da-vinci-resolve
 wine
 libreoffice-full
@@ -86,23 +90,55 @@ stellarium
 pinta
 mypaint
 shotwell
-nodejs'''
-	
+nodejs
+'''
+
 
 @click.group()
-@click.version_option('0.2.1', prog_name= 'Lola')
+@click.version_option('0.2.1', prog_name='Lola')
 def main():
-	'''I am Lola! Your assistant who can help you setup your Linux in an easy way! You can know more about me in https://github.com/arghyagod-coder/lola.
+    '''I am Lola! Your assistant who can help you setup your Linux in an easy way! You can know more about me in https://github.com/arghyagod-coder/lola.
 
 I can help you install apps through terminal, and you need to know almost nothing about the terminal to do so! Just simple prompts will be enough'''
-	pass
+    pass
 
-@main.command('check-apps', help='Check the list of supported apps!')
-def checkapps():
-	print(apps)
 
-@main.command('install', help= ('Install an app'))
-@click.argument('filesl', nargs=-1)
-def install(filesl): 
-	for files in filesl:
-		run(f'cd ~/Downloads; wget https://raw.githubusercontent.com/arghyagod-coder/lola/master/lolacli/scripts/{files}.sh; bash {files}.sh; rm {files}.sh')
+@main.command('list', help='Check the list of supported apps!')
+def list():
+    print(apps)
+
+
+@main.command('install', help=('Install an app'))
+@click.argument('files', nargs=-1)
+def install(files):
+    for file in files:
+        try:
+            # * changing working directory to the downloads dir
+            os.chdir(os.path.join(os.path.expanduser("~"), "Downloads"))
+
+            # * requesting the installation script
+            resp = requests.get(
+                f"https://raw.githubusercontent.com/arghyagod-coder/lola/master/lolacli/scripts/{file}.sh").text
+
+            # * writing response text into a file
+            with open(f"{file}_install.sh", 'w') as f:
+                f.write(resp)
+
+            with open(f'{file}_install.sh') as script_file:
+                script = script_file.read()
+                if script == "404: Not Found":
+                    raise FileNotFoundError()
+
+            # * running and then removing the bash script
+            run(f"bash {os.path.join(os.getcwd(), file)}_install.sh")
+            os.remove(os.path.join(os.getcwd(), f"{file}_install.sh"))
+
+        except FileNotFoundError:
+            print('This app is not available to download via lola yet :(')
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
