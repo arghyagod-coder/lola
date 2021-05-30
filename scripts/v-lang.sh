0c2e1099865a04d130bee8e8499f87cbd13364df
Arghya <arghyasarkar.joker@gmail.com>
2021-05-30 15:11:40 +0530

    Scripts Fixed

--- scripts/v-lang.sh
+++ scripts/v-lang.sh
@@ -1,4 +1,4 @@
-cd ~
-wget https://raw.githubusercontent.com/vlang/v/releases/latest/download/v_linux.zip 
-unzip ~/v_linux.zip
+cd ~;
+wget https://raw.githubusercontent.com/vlang/v/releases/latest/download/v_linux.zip ;
+unzip ~/v_linux.zip;
 sudo echo "PATH=$PATH:$HOME/v_linux" >> /etc/environment
