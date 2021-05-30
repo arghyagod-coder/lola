d1fbaf148a310afc84ad750f72d36d8489257510
Arghya <arghyasarkar.joker@gmail.com>
2021-05-30 15:26:58 +0530

    Scripts Fixed

--- scripts/v-lang.sh
+++ scripts/v-lang.sh
@@ -1,4 +1,4 @@
 cd ~;
 wget https://github.com/vlang/v/releases/latest/download/v_linux.zip ;
 unzip ~/v_linux.zip;
-sudo echo "PATH=$PATH:$HOME/v_linux" >> ~/.$0rc
+sudo echo "export PATH=$PATH:$HOME/v_linux" >> ~/.$0rc
